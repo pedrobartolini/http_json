60d02cc685599bd5548783f8e1145928f754f8ec
pedrobartolini <pedrobartolini@outlook.com>
2023-11-14 17:35:16 -0300

    NOTE: SMOL_THREADS VAR

--- src/direct_http.rs
+++ src/direct_http.rs
@@ -49,9 +49,7 @@ impl Server {
 
 	pub fn listen(self, router: Router, limiter: Limiter) {
 		limiter.start();
-
 		let router: &'static Router = Box::leak(Box::new(router));
-
 		smol::spawn(async move { self.client_acceptor(&router, limiter).await }).detach();
 	}
 
@@ -68,46 +66,69 @@ impl Server {
 }
 
 const BUFFER_SIZE: usize = 1024;
+const TIMEOUT: u64 = 2;
 
 async fn handler(mut stream: smol::Async<TcpStream>, addr: SocketAddr, router: &'static Router, limiter: Limiter) -> Result<()> {
 	let mut buffer = [0; BUFFER_SIZE];
 
-	// TODO INSERT READ TIMEOUT
-
 	let ip: &'static str = Box::leak(addr.ip().to_string().into_boxed_str());
 
 	loop {
-		if stream.read(&mut buffer).await? == 0 {
+		let mut keep_alive = false;
+
+		if stream.read_timeout().await || stream.read(&mut buffer).await? == 0 {
 			break;
 		}
 
-		if !limiter.server_allow() || !limiter.client_allow(&ip) {
-			stream.write(Response::status(429).message("Muitas requisições foram feitas. Tente novamente em alguns segundos.").finish().as_bytes()).await?;
-
-			stream.flush().await?;
-		} else {
-			let mut request = match Request::new(buffer) {
-				Some(request) => request,
-				None => {
-					continue;
-				}
-			};
-
-			let response = router::handle(&mut request, &router);
-
-			stream.write(response.finish().as_bytes()).await?;
-
-			stream.flush().await?;
-
-			if let Some(connection) = request.headers.get("connection") {
-				if connection == "keep-alive" {
-					continue;
-				} else {
-					break;
+		let response = {
+			if !limiter.server_allow() || !limiter.client_allow(&ip) {
+				Response::status(429).message("Muitas requisições foram feitas. Tente novamente em alguns segundos.").finish()
+			} else {
+				match Request::new(buffer) {
+					Some(mut request) => {
+						keep_alive = request.headers.get("connection").is_some_and(|value| value == "keep-alive");
+						router::handle(&mut request, &router).finish()
+					}
+					None => {
+						continue;
+					}
 				}
 			}
+		};
+
+		if stream.write_timeout().await {
+			break;
+		}
+
+		stream.write(response.as_bytes()).await?;
+
+		stream.flush().await?;
+
+		if !keep_alive {
+			break;
 		}
 	}
 
 	stream.close().await
+}
+
+#[async_trait::async_trait]
+trait Timeout {
+	async fn write_timeout(&self) -> bool;
+	async fn read_timeout(&self) -> bool;
+}
+
+#[async_trait::async_trait]
+impl Timeout for smol::Async<TcpStream> {
+	async fn write_timeout(&self) -> bool {
+		smol::future::race(async { Ok(self.writable()) }, async { Err(timer(TIMEOUT).await) }).await.is_err()
+	}
+
+	async fn read_timeout(&self) -> bool {
+		smol::future::race(async { Ok(self.readable()) }, async { Err(timer(TIMEOUT).await) }).await.is_err()
+	}
+}
+
+async fn timer(seconds: u64) {
+	smol::Timer::after(std::time::Duration::from_secs(seconds)).await;
 }
